df395b9e16e0a9bed877847357dec856c452509c
Zane Shelby <zane@users.noreply.github.com>
2022-01-28 21:25:59 -0500

    refactor: Fully parse airport output

--- src/io/zane/wifi.clj
+++ src/io/zane/wifi.clj
@@ -1,15 +1,39 @@
 (ns io.zane.wifi
   (:require [clojure.core.match :as match]
+            [clojure.string :as string]
             [clojure.java.shell :as shell]))
 
-(def ^:private airport
-  "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport")
+(defn airport-info
+  []
+  (let [parse-int #(Integer/parseInt %)
+        parsers {:lastTxRate parse-int
+                 :guardInterval parse-int
+                 :agrExtRSSI parse-int
+                 :agrCtlRSSI parse-int
+                 :agrCtlNoise parse-int
+                 :NSS parse-int
+                 :MCS parse-int
+                 :agrExtNoise parse-int
+                 :lastAssocStatus parse-int
+                 :maxRate parse-int}
+        rf (fn [acc s]
+             (if-let [[_ k v] (re-find #"^[\s]*(.*):[\s]*(.*[^\s])[\s]*" s)]
+               (if (string/blank? v)
+                 acc
+                 (let [k (keyword (string/replace k #"[\s]" "-"))
+                       v (if-let [f (get parsers k)]
+                           (f v)
+                           v)]
+                   (assoc acc k v)))
+               acc))]
+    (match/match (shell/sh "airport" "-I")
+      {:exit 0 :out out}
+      (reduce rf {} (string/split-lines out)))))
 
 (defn connected?
   []
-  (match/match (shell/sh "bash" "-c" (str airport " -I | grep \"state: running\""))
-    {:exit 0} true
-    :else false))
+  (let [{:keys [state]} (airport-info)]
+    (= "running" state)))
 
 (def disconnected? (complement connected?))
 
